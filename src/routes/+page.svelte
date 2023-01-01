a7f4f56981cae0acbd534b7ab22d46c0d34d433c
henriavo <3028339+henriavo@users.noreply.github.com>
2023-01-01 16:00:12 -0600

    added static header. added photo album page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+.text-small {
+  font-size: 85%;
+}
+
+.bi {
+vertical-align: -.125em;
+fill: currentColor;
+}
+</style>
